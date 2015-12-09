87f301e831aa905684d194e662cecf90547e049c
Dan Mossing <dan.mossing@gmail.com>
2015-12-08 19:23:46 -0800

    Com
    melted

--- aesthetic_sp_dim.m
+++ aesthetic_sp_dim.m
@@ -1,4 +1,6 @@
 function [h,w] = aesthetic_sp_dim(N)
+% spits out height and width for subplot so that width/height approximates
+% the golden ratio, given a required number of subplots.
 phi = (1+sqrt(5))/2;
 L = ceil(sqrt(N/phi));
 h = L;
